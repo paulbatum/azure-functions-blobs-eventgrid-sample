40a99e10d6d7b7d2fccdb9ccc871590141fbb4b3
Paul Batum <paul.batum@gmail.com>
2018-12-26 16:41:37 -0800

    Use EventGrid SDK

--- FuncBlobs.EventWriter/EventWriter.cs
+++ FuncBlobs.EventWriter/EventWriter.cs
@@ -6,6 +6,8 @@ using System.Text;
 using System.Threading.Tasks;
 using FuncBlobs.Formats;
 using FuncBlobs.Models;
+using Microsoft.Azure.EventGrid;
+using Microsoft.Azure.EventGrid.Models;
 using Microsoft.Azure.EventHubs;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
@@ -18,7 +20,7 @@ using Newtonsoft.Json.Linq;
 namespace FuncBlobs.EventWriter
 {
     public static class EventWriter
-    {
+    {        
         private static ThermostatLogFormatter formatter = new ThermostatLogFormatter();
         private static ConcurrentDictionary<string, CloudBlobClient> blobClients = new ConcurrentDictionary<string, CloudBlobClient>();
 
@@ -31,41 +33,41 @@ namespace FuncBlobs.EventWriter
             var containerName = Environment.GetEnvironmentVariable("ContainerName") ?? "container01";
 
             foreach (var eventGridPayload in events)
-            {
-                log.LogInformation("EVENT");
-                var payloadString = Encoding.UTF8.GetString(eventGridPayload.Body.Array);
-                var eventArray = JToken.Parse(payloadString);
-                foreach (dynamic e in eventArray)
-                {
-                    string eventType = e.eventType;
-                    if (eventType != "Microsoft.Storage.BlobCreated")
+            {                
+                var subscriber = new EventGridSubscriber();
+                var eventGridEvents = subscriber.DeserializeEventGridEvents(Encoding.UTF8.GetString(eventGridPayload.Body.Array));
+
+
+                
+                foreach (EventGridEvent e in eventGridEvents)
+                {                    
+                    if (e.EventType != EventTypes.StorageBlobCreatedEvent)
                     {
-                        log.LogWarning($"Recieved event other than blob create, got: '{eventType}', skipping.");
+                        log.LogWarning($"Recieved event other than blob create, got: '{e.EventType}', skipping.");
                         continue;
                     }
 
-                    var blobUri = new Uri((string)e.data.url);
+                    var blobCreatedEvent = (StorageBlobCreatedEventData) e.Data;
+                    
+                    var blobUri = new Uri(blobCreatedEvent.Url);
                     var filename = blobUri.Segments.Last();
                     if (filename.EndsWith(".csv") == false)
                     {
                         log.LogWarning($"Recieved event for non-csv file '{filename}', skipping.");
                         continue;
                     }
-
-                    string subject = e.subject;
-                    if(subject.StartsWith($"/blobServices/default/containers/{containerName}") == false)
+                    
+                    if(e.Subject.StartsWith($"/blobServices/default/containers/{containerName}") == false)
                     {
                         log.LogInformation($"Received event for unmonitored container, skipping.");
                         continue;
-                    }
-
-                    string topic = e.topic;
+                    }                    
                     
-                    string accountName = topic.Substring(topic.LastIndexOf('/') + 1);
+                    string accountName = e.Topic.Substring(e.Topic.LastIndexOf('/') + 1);
 
                     var blobClient = blobClients.GetOrAdd(accountName, (accName) =>
                     {
-                        string connectionEnvironmentVariableName = $"STORAGECONNECTION_{accountName}";
+                        string connectionEnvironmentVariableName = $"STORAGECONNECTION_{accName}";
                         string connectionString = Environment.GetEnvironmentVariable(connectionEnvironmentVariableName)
                             ?? throw new Exception($"Cannot find connection string environment variable '{connectionEnvironmentVariableName}'.");
 
