83b9fc1ef7e6216a0761c60eb9788e9b0c2a7009
Paul Batum <paul.batum@gmail.com>
2018-12-28 11:26:31 -0800

    Add error handling for event hubs -> cosmos

--- FuncBlobs.EventWriter/EventWriter.cs
+++ FuncBlobs.EventWriter/EventWriter.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -21,6 +22,8 @@ namespace FuncBlobs.EventWriter
 {
     public static class EventWriter
     {        
+        private static readonly string containerName = Environment.GetEnvironmentVariable("ContainerName") ?? "container01";
+        private static EventGridSubscriber subscriber = new EventGridSubscriber();
         private static ThermostatLogFormatter formatter = new ThermostatLogFormatter();
         private static ConcurrentDictionary<string, CloudBlobClient> blobClients = new ConcurrentDictionary<string, CloudBlobClient>();
 
@@ -28,70 +31,100 @@ namespace FuncBlobs.EventWriter
         public static async Task Run(
             [EventHubTrigger("%HubName%", Connection = "EventsConnectionString")] EventData[] events,
             [CosmosDB("%CosmosDatabaseName%", "%CosmosCollectionName%", ConnectionStringSetting = "CosmosConnectionString")] IAsyncCollector<string> thermostatLogOutput,
+            [Queue("blobevents-deadletter")] IAsyncCollector<string> deadLetterOutput,
             ILogger log)
         {
-            var containerName = Environment.GetEnvironmentVariable("ContainerName") ?? "container01";
+            var exceptions = new List<Exception>();
 
-            foreach (var eventGridPayload in events)
-            {                
-                var subscriber = new EventGridSubscriber();
-                var eventGridEvents = subscriber.DeserializeEventGridEvents(Encoding.UTF8.GetString(eventGridPayload.Body.Array));
+            foreach (EventData eventGridPayload in events)
+            {
+                // Safe to assume that our event hub will only have UTF8 messages
+                var messageBody = Encoding.UTF8.GetString(eventGridPayload.Body.Array, eventGridPayload.Body.Offset, eventGridPayload.Body.Count);
 
-
-                
-                foreach (EventGridEvent e in eventGridEvents)
+                try
                 {                    
-                    if (e.EventType != EventTypes.StorageBlobCreatedEvent)
-                    {
-                        log.LogWarning($"Recieved event other than blob create, got: '{e.EventType}', skipping.");
-                        continue;
-                    }
-
-                    var blobCreatedEvent = (StorageBlobCreatedEventData) e.Data;
-                    
-                    var blobUri = new Uri(blobCreatedEvent.Url);
-                    var filename = blobUri.Segments.Last();
-                    if (filename.EndsWith(".csv") == false)
-                    {
-                        log.LogWarning($"Recieved event for non-csv file '{filename}', skipping.");
-                        continue;
-                    }
-                    
-                    if(e.Subject.StartsWith($"/blobServices/default/containers/{containerName}") == false)
-                    {
-                        log.LogInformation($"Received event for unmonitored container, skipping.");
-                        continue;
-                    }                    
-                    
-                    string accountName = e.Topic.Substring(e.Topic.LastIndexOf('/') + 1);
-
-                    var blobClient = blobClients.GetOrAdd(accountName, (accName) =>
-                    {
-                        string connectionEnvironmentVariableName = $"STORAGECONNECTION_{accName}";
-                        string connectionString = Environment.GetEnvironmentVariable(connectionEnvironmentVariableName)
-                            ?? throw new Exception($"Cannot find connection string environment variable '{connectionEnvironmentVariableName}'.");
-
-                        CloudStorageAccount account = CloudStorageAccount.Parse(connectionString);
-                        return account.CreateCloudBlobClient();
-                    });
-
-                    var blob = await blobClient.GetBlobReferenceFromServerAsync(blobUri);
-                    using (var stream = await blob.OpenReadAsync(AccessCondition.GenerateEmptyCondition(), new BlobRequestOptions(), new OperationContext()))
-                    using (var reader = new StreamReader(stream))
-                    {
-                        ThermostatLog thermostatLog = ThermostatLog.ParseFileIdentifier(Path.GetFileNameWithoutExtension(filename));
-
-                        var csvUtils = new CsvUtils();
-                        thermostatLog.Readings = csvUtils.FromCsv(reader);
-                        string json = formatter.ToJson(thermostatLog);
-
-                        await thermostatLogOutput.AddAsync(json);
-                        await thermostatLogOutput.FlushAsync();
-                        log.LogInformation($"Wrote {filename} to cosmos.");
-                    }
+                    var eventGridEvents = subscriber.DeserializeEventGridEvents(messageBody);
+                    await ProcessEvents(eventGridEvents, thermostatLogOutput, log);
                 }
+                catch(Exception e)
+                {
+                    // We need to keep processing the rest of the batch - deadletter this particular failure and continue
+                    await deadLetterOutput.AddAsync(messageBody);                    
+                    exceptions.Add(e);
+                }                
             }
 
+            exceptions.ThrowIfAny();
+        }
+
+        private static async Task ProcessEvents(IEnumerable<EventGridEvent> eventGridEvents, IAsyncCollector<string> thermostatLogOutput, ILogger log)
+        {            
+            foreach (EventGridEvent e in eventGridEvents)
+            {
+                if (e.EventType != EventTypes.StorageBlobCreatedEvent)
+                {
+                    log.LogWarning($"Recieved event other than blob create, got: '{e.EventType}', skipping.");
+                    continue;
+                }
+
+                if (e.Subject.StartsWith($"/blobServices/default/containers/{containerName}") == false)
+                {
+                    log.LogInformation($"Received event for unmonitored container, skipping.");
+                    continue;
+                }
+
+                var blobCreatedEvent = (StorageBlobCreatedEventData)e.Data;
+
+                var blobUri = new Uri(blobCreatedEvent.Url);
+                var filename = blobUri.Segments.Last();
+                if (filename.EndsWith(".csv") == false)
+                {
+                    log.LogWarning($"Recieved event for non-csv file '{filename}', skipping.");
+                    continue;
+                }
+
+
+                string accountName = e.Topic.Substring(e.Topic.LastIndexOf('/') + 1);
+
+                var blobClient = blobClients.GetOrAdd(accountName, (accName) =>
+                {
+                    string connectionEnvironmentVariableName = $"STORAGECONNECTION_{accName}";
+                    string connectionString = Environment.GetEnvironmentVariable(connectionEnvironmentVariableName)
+                        ?? throw new Exception($"Cannot find connection string environment variable '{connectionEnvironmentVariableName}'.");
+
+                    CloudStorageAccount account = CloudStorageAccount.Parse(connectionString);
+                    return account.CreateCloudBlobClient();
+                });
+
+                var blob = await blobClient.GetBlobReferenceFromServerAsync(blobUri);
+                using (var stream = await blob.OpenReadAsync(AccessCondition.GenerateEmptyCondition(), new BlobRequestOptions(), new OperationContext()))
+                using (var reader = new StreamReader(stream))
+                {
+                    ThermostatLog thermostatLog = ThermostatLog.ParseFileIdentifier(Path.GetFileNameWithoutExtension(filename));
+
+                    var csvUtils = new CsvUtils();
+                    thermostatLog.Readings = csvUtils.FromCsv(reader);
+                    string json = formatter.ToJson(thermostatLog);
+
+                    await thermostatLogOutput.AddAsync(json);
+                    await thermostatLogOutput.FlushAsync();
+                    log.LogInformation($"Wrote {filename} to cosmos.");
+                }              
+            }
+
+
+        }
+    }
+
+    public static class EventWriterExtensions
+    {
+        public static void ThrowIfAny(this IEnumerable<Exception> exceptions)
+        {
+            if (exceptions.Count() > 1)
+                throw new AggregateException(exceptions);
+
+            if (exceptions.Count() == 1)
+                throw exceptions.Single();
         }
     }
 }
